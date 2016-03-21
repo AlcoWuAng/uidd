b75a903afc4dc3cf5208c62d5b2c503700262fcb
alcowuang <st40235@gmail.com>
2016-03-21 23:27:32 +0800

    +javascript&jquery

---
+++ info.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("#more-btn").click(function () {
+
+        $(".info2").animate({ top: "150px" }, 200);
+
+        $(".info").css({"position":"fixed" });
+
+    	$(window).on('scroll', function () {
+		    var pixs = $(document).scrollTop()
+		    pixs = pixs / 10;
+		    $(".infoimg").css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)" })     
+		});
+    });
+
+});
--- map.js
+++ map.js
@@ -1,7 +1,31 @@
 function initMap() {
-var mapDiv = document.getElementById('map');
-var map = new google.maps.Map(mapDiv, {
-  center: {lat: 22.9934, lng: 120.1979},
-  zoom: 16
-});
+    var market = { lat: 22.9934, lng: 120.1979 };
+    var mapDiv = document.getElementById('map');
+
+    var map = new google.maps.Map(mapDiv, {
+        center: market,
+        zoom: 16
+    });
+
+    var marker = new google.maps.Marker({
+        position: market,
+        map: map,
+        title: 'intro1',
+        icon:"img/pin.png"
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        $(".info").fadeIn();
+    });
+
+    $(".here-btn").click(function () {
+        map.setCenter(market);
+    });
 }
+
+
+
+
+
+
+
